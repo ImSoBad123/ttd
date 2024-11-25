cae51d81ac1f2988d77a0fea7d71c5fc5986d664
ImSoBad123 <138328968+ImSoBad123@users.noreply.github.com>
2024-11-25 20:15:30 +0700

    Create namngu.lua

---
+++ namngu.lua
@@ -0,0 +1,14 @@
+local idsend = 7519483358
+
+local args = {
+    [1] = {
+        [1] = {
+            [1] = "\226\129\130f",
+            [2] = "GoldenGladiatorCrate",
+            [3] = "Buy1",
+            [4] = idsend
+        }
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("NetworkingContainer"):WaitForChild("DataRemote"):FireServer(unpack(args))
